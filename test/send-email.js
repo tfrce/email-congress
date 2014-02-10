fd6bb421a04bcf5611dac88b66caf70b2e77a7d0
Beau Gunderson <beau@beaugunderson.com>
2014-02-10 01:19:44 -0800

    Fix test, add template

--- test/send-email.js
+++ test/send-email.js
@@ -1,3 +1,5 @@
+var should = require('chai').should();
+
 var fs = require('fs');
 var mailgun = require('mailgun-js')(process.env.MAILGUN_API_KEY,
                                     process.env.MAILGUN_DOMAIN);
@@ -6,7 +8,7 @@ var Mustache = require('mustache');
 var domesticTemplate = fs.readFileSync('templates/domestic.html', 'utf8');
 
 describe('mailgun', function () {
-  it('should send an email', function () {
+  it('should send an email', function (cb) {
     var details = {
       user: 'asdas'
     };
@@ -15,11 +17,17 @@ describe('mailgun', function () {
       from: 'Excited User <me@samples.mailgun.org>',
       to: process.env.TEST_EMAIL || 'thomasalwyndavis@gmail.com',
       subject: 'Hello',
-      text: Mustache.render(domesticTemplate, details)
+      html: Mustache.render(domesticTemplate, details)
     };
 
-    mailgun.messages.send(data, function (error, response, body) {
-      console.log(body);
+    mailgun.messages.send(data, function (err, response, body) {
+      should.not.exist(err);
+
+      body.message.should.equal('Queued. Thank you.');
+
+      should.exist(body.id);
+
+      cb();
     });
   });
 });
